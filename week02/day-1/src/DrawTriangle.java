b9a4de736aeca42ef2796c352a2d285a7ddfd89d
nezih <nezihcihan@hotmail.com>
2017-09-12 00:53:38 +0200

    .gitignore file added

--- week02/day-1/src/DrawTriangle.java
+++ week02/day-1/src/DrawTriangle.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class DrawTriangle {
     public static void main(String[] args) {
         // Write a program that reads a number from the standard input, then draws a
-// triangle like this:
-// The triangle should have as many lines as the number was
+        // triangle like this:
+        // The triangle should have as many lines as the number was
         System.out.println("Enter a number for your triangle!\n");
         Scanner input = new Scanner(System.in);
         int a = input.nextInt();
