5ba75b794fe6f74ebb34778d17743d1c34fe3af6
nezih <nezihcihan@hotmail.com>
2017-09-11 13:37:52 +0200

    correction over exercise1 and 2

---
+++ week02/day-1/weekone/src/HumptyDumpty.java
@@ -0,0 +1,9 @@
+public class HumptyDumpty {
+    public static void main(String[] args) {
+        // Modify this program to print Humpty Dumpty riddle correctly
+        System.out.println("Humpty Dumpty sat on a wall,");
+        System.out.println("Humpty Dumpty had a great fall.");
+        System.out.println("Four-score Men and Four-score more,");
+        System.out.println("Could not make Humpty Dumpty where he was before.");
+    }
+}
