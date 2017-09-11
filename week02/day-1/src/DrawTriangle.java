0abe28c8ab68eb06d26c10dda182f4a24d8e69a7
nezih <nezihcihan@hotmail.com>
2017-09-11 17:19:30 +0200

    week02 day2 exercises

---
+++ week02/day-1/src/DrawTriangle.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DrawTriangle {
+    public static void main(String[] args) {
+        // Write a program that reads a number from the standard input, then draws a
+// triangle like this:
+// The triangle should have as many lines as the number was
+        System.out.println("Enter a number for your triangle!\n");
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        for(int i = 0; i <= a; i++) {
+            for (int j = 0; j <= a; j++) {
+                System.out.println("*");
+            }
+            System.out.println("\n");
+        }
+    }
+}
+
--- week02/day-1/weekone/src/PrintEven.java
+++
@@ -1,7 +0,0 @@
-public class PrintEven {
-    public static void main(String[] args) {
-        for (int i = 0; i <= 500; i = i + 2) {
-            System.out.println(i);
-        }
-    }
-}
