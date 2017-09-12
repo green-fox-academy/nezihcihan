75ef40d7a3b3cf641d48fb7fc1aad48d19c954b1
nezih <nezihcihan@hotmail.com>
2017-09-12 15:45:53 +0200

    Printer,java modified

--- week02/day-2/src/Printer.java
+++ week02/day-2/src/Printer.java
@@ -3,7 +3,7 @@ public class Printer {
         // - Create a function called `printer`
         //   which prints the input String parameters
         // - It can have any number of parameters
-        printer("first" , "second", "third");
+        printer("first" , "second", "third", "four", "five","six", "seven", "eight", "and so on...");
     }
     public static void printer(String ... strings) {
         for (String s : strings)
