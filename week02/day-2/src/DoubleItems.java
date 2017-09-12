5e207ed9c1935c849002eab95be9780a4800bb81
nezih <nezihcihan@hotmail.com>
2017-09-12 16:12:39 +0200

    exc - DoubleItems

---
+++ week02/day-2/src/DoubleItems.java
@@ -0,0 +1,16 @@
+// - Create an array variable named `ag`
+//   with the following content: `[3, 4, 5, 6, 7]`
+// - Double all the values in the array
+
+public class DoubleItems {
+    public static void main(String[] args) {
+        int [] ag = {3, 4, 5, 6, 7};
+        for(int i = 0; i < ag.length; i++) {
+            ag[i] *= 2;
+        }
+        for(int j = 0; j < ag.length; j++) {
+            System.out.print(ag[j] + ", ");
+        }
+    }
+}
+
