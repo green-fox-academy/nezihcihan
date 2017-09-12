90adea318a1e0a62ee8b9caedc481c62dd3b7d20
nezih <nezihcihan@hotmail.com>
2017-09-12 13:27:20 +0200

    01.Doubling.java

--- week02/day-2/src/Doubling.java
+++ week02/day-2/src/Doubling.java
@@ -1,2 +1,12 @@
 public class Doubling {
+    public static void main(String[] args) {
+        // - Create an integer variable named `ak` and assign the value `123` to it
+        // - Create a function called `doubling` that doubles it's input parameter
+        // - Print the result of `doubling(ak)`
+        int ak = 123;
+        System.out.println(doubleFunction(ak));
+    }
+    public static int doubleFunction(int number) {
+        return number * 2;
+    }
 }
