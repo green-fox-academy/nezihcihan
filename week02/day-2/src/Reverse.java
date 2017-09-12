bd156772a8362441b2ceac54bb5d9ededac41e85
nezih <nezihcihan@hotmail.com>
2017-09-12 20:04:13 +0200

    Exc.19 Reverse modified and shortened

--- week02/day-2/src/Reverse.java
+++ week02/day-2/src/Reverse.java
@@ -5,14 +5,14 @@
 public class Reverse {
     public static void main(String[] args) {
         int [] aj = {3, 4, 5, 6, 7};
-        
-        for(int i = 0; i < aj.length; i++) {
+
+        for(int i = 0; i < aj.length/2 ; i++) {
             int temp = aj[i];
-            aj[i] = aj[aj.length - i - 1];
-            aj[aj.length - i - 1] = temp;
+            aj[i] = aj[aj.length - 1 - i];
+            aj[aj.length - 1 - i] = temp;
         }
         for(int j = 0; j < aj.length; j++) {
-            System.out.println(aj[j]);
+            System.out.print(aj[j] + ", ");
         }
     }
 }
