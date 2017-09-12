c073f5914ea5bce716b97b76f7eb46f88369247d
nezih <nezihcihan@hotmail.com>
2017-09-12 15:37:11 +0200

    Sum.java modified

---
+++ week02/day-2/src/DiagonalMatrix.java
@@ -0,0 +1,16 @@
+// - Create (dynamically) a two dimensional array
+//   with the following matrix. Use a loop!
+//
+//   1 0 0 0
+//   0 1 0 0
+//   0 0 1 0
+//   0 0 0 1
+//
+// - Print this two dimensional array to the output
+public class DiagonalMatrix {
+    public static void main(String[] args) {
+        int [] [] twoDimension = new int[4][4];
+
+
+    }
+}
--- week02/day-2/src/Sum.java
+++ week02/day-2/src/Sum.java
@@ -3,16 +3,14 @@ public class Sum {
     // - Write a function called `sum` that sum all the numbers
     //   until the given parameter
     public static void main(String[] args) {
-        total();
+        sum(6);
     }
-    public static int total() {
-        System.out.println("enter three numbers");
-        Scanner input = new Scanner(System.in);
-        int firstNum = input.nextInt();
-        int secondNum = input.nextInt();
-        int thirdNum = input.nextInt();
-        int inTotal = firstNum + secondNum + thirdNum;
-        System.out.println(inTotal);
-        return inTotal;
+    public static  int sum(int till){
+        int sumOfAll = 0;
+        for(int i =0; i < till; i++){
+        sumOfAll += i;
+        }
+        System.out.println(sumOfAll);
+        return sumOfAll;
     }
 }
