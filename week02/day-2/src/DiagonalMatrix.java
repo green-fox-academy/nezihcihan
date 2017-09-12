3568d66555456483092b7038d1f7cfe94b17b77c
nezih <nezihcihan@hotmail.com>
2017-09-12 16:04:27 +0200

    exc. DiagonalMatrix added

--- week02/day-2/src/DiagonalMatrix.java
+++ week02/day-2/src/DiagonalMatrix.java
@@ -9,8 +9,22 @@
 // - Print this two dimensional array to the output
 public class DiagonalMatrix {
     public static void main(String[] args) {
-        int [] [] twoDimension = new int[4][4];
-
-
+        int [] [] arr = new int[4][4];
+        for(int row = 0; row < 4; row++){
+            for(int col = 0; col < 4; col++) {
+                if(col == row){
+                    arr[row][col] = 1;
+                }
+                else {
+                    arr[row][col] = 0;
+                }
+            }
+        }
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
