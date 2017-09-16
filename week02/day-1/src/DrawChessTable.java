e4f00002b6cd3a513eacf39338b7f36e2740cdeb
nezih <nezihcihan@hotmail.com>
2017-09-16 20:30:59 +0200

    ChessTable function added

--- week02/day-1/src/DrawChessTable.java
+++ week02/day-1/src/DrawChessTable.java
@@ -3,22 +3,28 @@
 //  % % % %
 // % % % %
 //  % % % %
-// % % % %
-//  % % % %
-// % % % %
-//  % % % %
+import java.util.Scanner;
 public class DrawChessTable {
     public static void main(String[] args) {
-        for (int i = 0; i < 8 ; i++) {
-            for (int j = 0; j < 4; j++){
-                if(i==0 || i % 2 == 0){
+    drawTable(userInput());
+    }
+    /**METHOD for User Input**/
+    public static int userInput(){
+        System.out.println("Enter the line number of your table.");
+        Scanner input = new Scanner(System.in);
+        int lines = input.nextInt();
+        return lines;
+    }
+    /**METHOD for Drawing Table**/
+    public static void drawTable(int lines){
+        for (int row = 0; row < lines; row++) {
+            for (int col = 0; col < lines/2; col++){
+                if(row==0 || row % 2 == 0){
                     System.out.print("% ");
                 }
                 else{
                     System.out.print(" %");
                 }
-
-
             }
             System.out.println();
         }
