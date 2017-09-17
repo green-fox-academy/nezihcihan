3f7cd2d17f8eb216ecad71d46472d1d584642c8f
nezih <nezihcihan@hotmail.com>
2017-09-18 00:45:04 +0200

    Josephus improved

--- week02/day-5/src/JosephusProblem.java
+++ week02/day-5/src/JosephusProblem.java
@@ -1,9 +1,31 @@
 import java.util.ArrayList;
 public class JosephusProblem {
     public static void main(String[] args) {
-        ArrayList<Integer> numOfPeople = new ArrayList<>(13);
-        for (int i = 0; i < numOfPeople.size() ; i++) {
+        int numOfPeople = 7;
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < numOfPeople ; i++) {
+            list.add("" + (1 + i));
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
 
+        while (list.size() > 1) {
+            if(list.size() % 2 != 0) {
+                for (int j = 0; j < list.size(); j= j+2) {
+                    list.remove(j+1);
+                }
+                //for (int i = 0; i < list.size() ; i = i + 2) {
+                //    list.remove(i);
+                //}
+            }
+            /**else {
+                for (int i = 1; i < list.size() ; i = i +2) {
+                    list.remove(i);
+                }
+            }**/
+        }
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
         }
     }
 }
