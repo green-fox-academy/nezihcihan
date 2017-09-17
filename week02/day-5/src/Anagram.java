d637b49797e6850158815513c9aad2ec17f2264a
nezih <nezihcihan@hotmail.com>
2017-09-17 15:24:25 +0200

    PalindromeBuilder funtions added & Anagram modified

--- week02/day-5/src/Anagram.java
+++ week02/day-5/src/Anagram.java
@@ -3,27 +3,22 @@ public class Anagram {
     public static void main(String[] args) {
         isAnagram("dog","god");
     }
-    static void isAnagram(String s1, String s2)
-    {
-        boolean status = true;
-        if(s1.length() != s2.length())
-        {
-            status = false;
+    /**Method For  **/
+    static void isAnagram(String s1, String s2) {
+        boolean status = false;
+        if(s1.length() != s2.length()) {
         }
-        else
-        {
+        else {
             char[] s1Array = s1.toLowerCase().toCharArray();
             char[] s2Array = s2.toLowerCase().toCharArray();
             Arrays.sort(s1Array);
             Arrays.sort(s2Array);
             status = Arrays.equals(s1Array, s2Array);
         }
-        if(status)
-        {
-            System.out.println(s1+" and "+s2+" are anagrams");
+        if(status) {
+            System.out.println(s1+" and "+s2+" are anagrams.");
         }
-        else
-        {
+        else {
             System.out.println(s1+" and "+s2+" are not anagrams");
         }
     }
--- week02/day-5/src/PalindromeBuilder.java
+++ week02/day-5/src/PalindromeBuilder.java
@@ -1,9 +1,18 @@
 import java.util.Scanner;
 public class PalindromeBuilder {
     public static void main(String[] args) {
+        palindromBuilder(userInput());
+    }
+    
+    /** METHOD FOR USER INPUT **/
+    public static String userInput() {
         System.out.println("Enter a word!");
         Scanner input = new Scanner(System.in);
         String word = input.nextLine();
+        return userInput();
+    }
+    /** METHOD FOR PALINDROME BUILDING **/
+    public static void palindromBuilder(String word) {
         char[] wordArray = word.toCharArray();
         char[] otherArray = new char[word.length()];
         for (int i = 0; i < word.length(); i++) {
