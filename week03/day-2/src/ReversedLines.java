c829f4a2b231d3f3c313d26922fc6706f09fadb6
nezih <nezihcihan@hotmail.com>
2017-09-19 21:33:11 +0200

    exc. ReversedLine added

--- week03/day-2/src/ReversedLines.java
+++ week03/day-2/src/ReversedLines.java
@@ -1,31 +1,28 @@
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-
 public class ReversedLines {
 
     public static void main(String[] args) {
         // Create a method that decrypts reversed-lines.txt
-        try { // Required by Files.readAllLines(filePath);
-                // Reads the content from `myFile.txt` in the `assets`
-            // folder line by line to a String List
-            Path filePath = Paths.get("reversed-lines.txt");
+        reverse("reversed-lines.txt");
+    }
+    public static void reverse (String fileName) {
+
+        try {
+            Path filePath = Paths.get(fileName);
             List<String> lines = Files.readAllLines(filePath);
             ArrayList<String> newList = new ArrayList<>();
-            String temp = "";
-            for (int i = 0; i < lines.size()-1; i++) {
-
-                for (int j = 0; j < ; j++) {
-
-                }
+            for (String line : lines) {
+                String reverse = new StringBuffer(line).reverse().toString();
+                newList.add(reverse);
+                System.out.println(reverse); // prints the line
             }
-            System.out.println(lines); // Prints the all lines of the file
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("Unable to read the file!");
         }
-
     }
 }
