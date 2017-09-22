1274fb258fa3eb8eec7688ad158ab5df94830ef7
nezih <nezihcihan@hotmail.com>
2017-09-22 09:43:22 +0200

    yellowThingy added

--- week03/day-3/src/Checkerboard.java
+++ week03/day-3/src/Checkerboard.java
@@ -7,17 +7,17 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class Checkerboard {
 
     public static void mainDraw(Graphics graphics){
-        int row;   // Row number, from 0 to 7
-        int col;   // Column number, from 0 to 7
-        int x,y;   // Top-left corner of square
+        int row;
+        int col;
+        int x,y;
         for ( row = 0;  row < 8;  row++ ) {
             for ( col = 0;  col < 8;  col++) {
                 x = col * 20;
                 y = row * 20;
                 if ( (row % 2) == (col % 2) )
-                    graphics.setColor(Color.white);
+                    graphics.setColor(Color.YELLOW);
                 else
-                    graphics.setColor(Color.black);
+                    graphics.setColor(Color.BLUE);
                 graphics.fillRect(x, y, 20, 20);
             }
         }
---
+++ week03/day-4/src/yellowTable.java
@@ -0,0 +1,83 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class yellowTable {
+    public static void mainDraw(Graphics graphics){
+        // reproduce this:
+        // [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps/r3.png]
+
+                int xCorner = 50;
+                int yCorner = 50;
+                int width = 600;
+                int height = 600;
+                graphics.setColor(Color.YELLOW);
+                graphics.fillRect(xCorner, yCorner, width, height);
+
+                lines(graphics, xCorner, yCorner, width, height);
+            }
+            public static void lines(Graphics graphics, int xCorner, int yCorner, int width, int height) {
+                int x1,  y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6, x7, y7, x8, y8;
+                if (width == 0 && height == 0) {
+                    return;
+                }
+                else {
+                    graphics.setColor(Color.black);
+                    x1 = xCorner + width / 3;
+                    x2 = xCorner + 2 * width / 3;
+                    y1 = yCorner;
+                    y2 = y1;
+
+                    x3 = x1;
+                    x4 = x2;
+                    y3 = yCorner + height;
+                    y4 = y3;
+                    graphics.drawLine(x1, y1, x3, y3);
+                    graphics.drawLine(x2, y2, x4, y4);
+
+                    x5 = xCorner;
+                    x6 = xCorner + width;
+                    y5 = yCorner + height / 3;
+                    y6 = y5;
+
+                    graphics.drawLine(x5, y5, x6, y6);
+                    x7 = x5;
+                    x8 = x6;
+                    y7 = yCorner + 2 * height / 3;
+                    y8 = y7;
+                    graphics.drawLine(x7, y7, x8, y8);
+
+                    lines(graphics, xCorner + width / 3, yCorner, width / 3, height / 3);
+                    lines(graphics,  xCorner, yCorner + width / 3, width / 3, height / 3);
+                    lines(graphics, xCorner + width / 3, yCorner + 2 * width / 3, width / 3, height / 3);
+                    lines(graphics, xCorner + 2 * width / 3, yCorner + width / 3, width / 3, height / 3);
+                }
+            }
+
+
+
+
+    //    Don't touch the code below
+    static int WIDTH = 600;
+    static int HEIGHT = 600;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+    static class ImagePanel extends JPanel{
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+
+        }
+    }
+
+}
