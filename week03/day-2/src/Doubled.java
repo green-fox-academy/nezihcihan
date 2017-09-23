7687fcebebe342d525fa9b8756bafd5d8a64061b
nezih <nezihcihan@hotmail.com>
2017-09-23 13:38:35 +0200

    Doubled improved

--- week03/day-2/src/Doubled.java
+++ week03/day-2/src/Doubled.java
@@ -1,37 +1,33 @@
-import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-
 public class Doubled {
-
     public static void main(String[] args) {
         // Create a method that decrypts the duplicated-chars.txt
-        ArrayList<Character> newList = new ArrayList<>();
-        ArrayList<String> last = new ArrayList<>();
+        System.out.println(decripts("duplicated-chars.txt"));
+    }
+    public static ArrayList<String> decripts(String file){
+        ArrayList<Character> charList = new ArrayList<>();
+        String last = "";
+        ArrayList<String> decriptedList = new ArrayList<>();
         try {
-            Path filePath = Paths.get("duplicated-chars.txt");
-            List<String> lines = Files.readAllLines(filePath);
-            for (String line : lines) {
-                for (int j = 0; j < (line.length()) ; j += 2) {
-                    newList.add(line.charAt(j));
+            Path filePath = Paths.get(file);
+            List<String> insideFile = Files.readAllLines(filePath);
+            for (String line : insideFile) {
+                for (int j = 0; j < line.length() ; j += 2) {
+                    charList.add(line.charAt(j));
                 }
             }
-            for (int k = 0; k < newList.size(); k++) {
-                last.add(newList.get(k).toString());
+            for (int k = 0; k < charList.size(); k++) {
+                last = last + charList.get(k);
             }
-            for (int i = 0; i < last.size() ; i++) {
-                System.out.print(last.get(i));
-                if (last.get(i).contentEquals(".")) {
-                    System.out.print(last.get(i) + "\n");
-                }
-            }
-
+            decriptedList.add(last);
         }
         catch (Exception e) {
             System.out.println("Unable to read the file!");
         }
+        return decriptedList;
     }
 }
