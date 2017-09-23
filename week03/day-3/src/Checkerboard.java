96f3d5c57140451773f7b95a56cef850e769e50d
nezih <nezihcihan@hotmail.com>
2017-09-23 20:56:07 +0200

    Checker board cleaned up

--- week03/day-3/src/Checkerboard.java
+++ week03/day-3/src/Checkerboard.java
@@ -3,34 +3,26 @@ import javax.swing.*;
 import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
-
 public class Checkerboard {
-
     public static void mainDraw(Graphics graphics){
         int row;
         int col;
-        int x,y;
-        for ( row = 0;  row < 8;  row++ ) {
-            for ( col = 0;  col < 8;  col++) {
-                x = col * 20;
-                y = row * 20;
-                if ( (row % 2) == (col % 2) )
+        int x, y;
+        for (row = 0; row < 8; row++) {
+            for (col = 0;  col < 8;  col++) {
+                x = col * 40;
+                y = row * 40;
+                if ((row % 2) == (col % 2))
                     graphics.setColor(Color.YELLOW);
                 else
                     graphics.setColor(Color.BLUE);
-                graphics.fillRect(x, y, 20, 20);
+                    graphics.fillRect(x, y, 40, 40);
             }
         }
     }
-
-
-
-
-
-//    Don't touch the code below
+    //Don't touch the code below
     static int WIDTH = 320;
     static int HEIGHT = 343;
-
     public static void main(String[] args) {
         JFrame jFrame = new JFrame("Drawing");
         jFrame.setSize(new Dimension(WIDTH, HEIGHT));
@@ -44,8 +36,6 @@ public class Checkerboard {
         protected void paintComponent(Graphics graphics) {
             super.paintComponent(graphics);
             mainDraw(graphics);
-
         }
     }
-
 }
