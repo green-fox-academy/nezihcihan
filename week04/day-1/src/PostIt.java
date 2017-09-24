db2e0661bcad27d3bea2da1cfa9824ceb6aad5f0
nezih <nezihcihan@hotmail.com>
2017-09-24 20:58:33 +0200

    PostIt class completed

---
+++ week04/day-1/src/PostIt.java
@@ -0,0 +1,19 @@
+public class PostIt {
+    String backgroundColor;
+    String text;
+    String textColor;
+
+    public PostIt(String backgroundColor, String text, String textColor){
+        this.backgroundColor = backgroundColor;
+        this.text = text;
+        this.textColor = textColor;
+    }
+    
+    public static void main(String[] args) {
+        PostIt example1 = new PostIt("Orange", "Idea 1", "Blue text");
+        PostIt example2 = new PostIt("Pink", "Awesome", "Black text");
+        PostIt example3 = new PostIt("Yellow", "Superb", "Green text");
+    }
+}
+
+
