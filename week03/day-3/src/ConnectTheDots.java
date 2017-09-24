64ebd9ae2e2e47fa5291af7fd550f2862894292a
nezih <nezihcihan@hotmail.com>
2017-09-24 16:08:08 +0200

    connect dots added

--- week03/day-3/src/ConnectTheDots.java
+++ week03/day-3/src/ConnectTheDots.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
@@ -11,12 +13,40 @@ public class ConnectTheDots {
       // an array of {x, y} points
       // and connects them with green lines.
       // connect these to get a box: {{10, 10}, {290,  10}, {290, 290}, {10, 290}}
-      // connect these: {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},
+      // connect these: {{{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},
       // {120, 100}, {85, 130}, {50, 100}}
 
+        int[] leftUp = {10, 10};
+        int[] rightUp = {290,10};
+        int[] leftDown = {290, 290};
+        int[] rightDown = {10, 290};
+        ArrayList<int[]> box = new ArrayList<>(Arrays.asList(leftUp,leftDown,rightDown,rightUp));
 
+        int[] dot1 = {50, 100};
+        int[] dot2 = {70, 70};
+        int[] dot3 = {80, 90};
+        int[] dot4 = {50, 100};
+        int[] dot5 = {100, 70};
+        int[] dot6 = {120, 100};
+        int[] dot7 = {85, 130};
+        int[] dot8 = {50, 100};
+        ArrayList<int[]> dots = new ArrayList<>(Arrays.asList(dot1,dot2,dot3,dot4,dot5,dot6,dot7,dot8));
+        graphics.setColor(Color.BLACK);
+        //drawDots(graphics, dots);
+        drawDots(graphics, box);
+    }
+    public static void drawDots (Graphics graphics,ArrayList<int[]> dots){
+        for (int i = 0; i < dots.size() ; i++) {
+
+        }
 
     }
+
+    //public static void dots(Graphics g,int[] a){
+      //  a = new int[8];
+        //g.setColor(Color.BLACK);
+
+    //}
 
 //    Don't touch the code below
     static int WIDTH = 320;
