7f18ac3982e1679c35f8befbe5bc30c6caa12b7f
nezih <nezihcihan@hotmail.com>
2017-09-24 10:28:09 +0200

    EnvelopeStar finished

--- week03/day-3/src/EnvelopeStar.java
+++ week03/day-3/src/EnvelopeStar.java
@@ -9,8 +9,38 @@ public class EnvelopeStar {
     public static void mainDraw(Graphics graphics){
       // reproduce this:
       // [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/envelope-star/r2.png]
-
-
+        int x2 = 300;
+        int y2 = 0;
+        for (int i = 0; i < 16; i++) {
+            graphics.setColor(Color.GREEN);
+            graphics.drawLine(300, y2, x2, 300);
+            x2 += 20;
+            y2 += 20;
+        }
+        int x = 0;
+        int y = 300;
+        for (int i = 0; i < 16; i++) {
+            graphics.setColor(Color.GREEN);
+            graphics.drawLine(x, 300, 300, y);
+            x += 20;
+            y += 20;
+        }
+        int x3 = 600;
+        int y3 = 300;
+        for (int i = 0; i < 16; i++) {
+            graphics.setColor(Color.GREEN);
+            graphics.drawLine(x3, 300, 300, y3);
+            x3 -= 20;
+            y3 += 20;
+        }
+        int x4 = 300;
+        int y4 = 0;
+        for (int i = 0; i < 16; i++) {
+            graphics.setColor(Color.GREEN);
+            graphics.drawLine(300, y4, x4, 300);
+            x4 -= 20;
+            y4 += 20;
+        }
 
 
     }
