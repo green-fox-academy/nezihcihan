b691349c4e5498999fc46d6f4f116559f07def32
nezih <nezihcihan@hotmail.com>
2017-09-24 20:35:25 +0200

    Trees homework added

---
+++ week04/day-1/src/Trees.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Trees {
+    public static void main(String[] args) {
+        ArrayList<Object> oak = new ArrayList<>(Arrays.asList("Oak", "5 years old","green leaf","male" ));
+        ArrayList<Object> cherry = new ArrayList<>(Arrays.asList("Cherry", "3 years old","green leaf","male" ));
+        ArrayList<Object> apple = new ArrayList<>(Arrays.asList("Apple", "7 years old","green leaf","female" ));
+        ArrayList<Object> walnut = new ArrayList<>(Arrays.asList("Walnut", "8 years old","green leaf","male" ));
+        ArrayList<Object> chestnut = new ArrayList<>(Arrays.asList("Chestnut", "10 years old","green leaf","male" ));
+    }
+}
