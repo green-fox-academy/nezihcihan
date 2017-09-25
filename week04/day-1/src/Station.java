7d06338cfc7fd8cd958abdd3b514c944e1eb8327
nezih <nezihcihan@hotmail.com>
2017-09-25 17:06:40 +0200

    Car & Station modified

--- week04/day-1/src/Station.java
+++ week04/day-1/src/Station.java
@@ -3,14 +3,15 @@ public class Station {
     int gasAmount;
     int capacity;
 
-    public void refill(){
-        Car myCar = new Car();
-        this.gasAmount = myCar.gasAmount +100;
-        this.capacity = myCar.capacity - 100;
+    public void refill(Car car){
+
+        this.gasAmount = car.gasAmount +100;
+        this.capacity = car.capacity - 100;
     }
     public static void main(String[] args) {
         Station myStation = new Station();
-        myStation.refill();
+        Car myCar = new Car();
+        myStation.refill(myCar);
         System.out.println("Car gasAmount after refill: " + myStation.gasAmount);
         System.out.println("Car refill capacity after refill: " + myStation.capacity);
     }
