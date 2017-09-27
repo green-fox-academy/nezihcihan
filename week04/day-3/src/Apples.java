7a68c3114b59676a7eefa7a05dce28e6c5a3bb05
nezih <nezihcihan@hotmail.com>
2017-09-27 10:31:44 +0200

    AppleTest finished

--- week04/day-3/src/Apples.java
+++ week04/day-3/src/Apples.java
@@ -1,2 +1,10 @@
 public class Apples {
+    private String fruit;
+
+    public Apples(){
+        this.fruit = "apples";
+    }
+    public String getApple() {
+        return fruit;
+    }
 }
--- week04/day-3/tests/ApplesTest.java
+++ week04/day-3/tests/ApplesTest.java
@@ -1,5 +1,17 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ApplesTest {
-
+    String fruit = "apples";
+    String expected = fruit;
+    Apples appleClass = new Apples();
+    String output = appleClass.getApple();
+    
+    @Test
+    public void testGetApple() throws Exception {
+        assertEquals(expected, output);
+    }
 }
