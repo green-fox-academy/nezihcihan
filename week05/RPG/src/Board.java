1896f70834314b61d5cc335bc41aae82ba2cd11c
nezih <nezihcihan@hotmail.com>
2017-10-03 17:34:19 +0200

    Skeleton and Boss Classes added

--- week05/RPG/src/Board.java
+++ week05/RPG/src/Board.java
@@ -14,12 +14,12 @@ import java.util.List;
 
 public class Board extends JComponent implements KeyListener {
     String heroImage = "hero-down.png";
-    int heroPosX;
-    int heroPosY;
-
+    int PosX;
+    int PosY;
+    List<String> content = new ArrayList<>();
     public Board() {
-        heroPosX = 0;
-        heroPosY = 0;
+        PosX = 0;
+        PosY = 0;
 
         // set the size of your draw board
         setPreferredSize(new Dimension(720, 720));
@@ -28,10 +28,10 @@ public class Board extends JComponent implements KeyListener {
     @Override
     public void paint(Graphics graphics) {
         super.paint(graphics);
-        graphics.fillRect(heroPosX, heroPosY, 100, 100);
+        graphics.fillRect(PosX, PosY, 100, 100);
         // here you have a 720x720 canvas
         // you can create and draw an image using the class below e.g.
-        List<String> content = new ArrayList<>();
+
         Path sourcePath = Paths.get("assets/board.txt");
         try {
             content = Files.readAllLines(sourcePath);
@@ -39,19 +39,19 @@ public class Board extends JComponent implements KeyListener {
             System.out.println("no content");
         }
 
-        for (int i = 0; i < content.size() ; i++) {
-            for (int j = 0; j < content.size() ; j++) {
-                    if (content.get(j).charAt(i) == '0') {
-                        PositionedImage image = new PositionedImage("assets/floor.png", i * 72, j * 72);
+        for (int row = 0; row < content.size() ; row++) {
+            for (int col = 0; col < content.size() ; col++) {
+                    if (content.get(col).charAt(row) == '0') {
+                        PositionedImage image = new PositionedImage("assets/floor.png", row * 72, col * 72);
                         image.draw(graphics);
                     }
-                    else if (content.get(j).charAt(i) == '1') {
-                        PositionedImage image = new PositionedImage("assets/wall.png", i * 72, j * 72);
+                    else if (content.get(col).charAt(row) == '1') {
+                        PositionedImage image = new PositionedImage("assets/wall.png", row * 72, col * 72);
                         image.draw(graphics);
                     }
             }
         }
-        PositionedImage hero = new PositionedImage("assets/"+heroImage, heroPosX, heroPosY);
+        PositionedImage hero = new PositionedImage("assets/"+ heroImage, PosX, PosY);
         hero.draw(graphics);
 
     }
@@ -83,19 +83,19 @@ public class Board extends JComponent implements KeyListener {
     public void keyReleased(KeyEvent e) {
         // When the up or down keys hit, we change the position of our box
         if (e.getKeyCode() == KeyEvent.VK_UP) {
-            heroPosY -= 72;
+            PosY -= 72;
             heroImage = "hero-up.png";
         }
         else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
-            heroPosY += 72;
+            PosY += 72;
             heroImage = "hero-down.png";
         }
         else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
-            heroPosX -= 72;
+            PosX -= 72;
             heroImage = "hero-left.png";
         }
-        else if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            heroPosX += 72;
+        else if(e.getKeyCode() == KeyEvent.VK_RIGHT ) {
+            PosX += 72;
             heroImage = "hero-right.png";
         }
         // and redraw to have a new picture with the new coordinates
