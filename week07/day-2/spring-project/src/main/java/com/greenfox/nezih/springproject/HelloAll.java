ae0734d39ec5ec6b20977e79cc1b0bd70aa1dff1
nezih <nezihcihan@hotmail.com>
2017-10-24 17:40:57 +0200

    GreetAll works with color

--- week07/day-2/spring-project/src/main/java/com/greenfox/nezih/springproject/HelloAll.java
+++ week07/day-2/spring-project/src/main/java/com/greenfox/nezih/springproject/HelloAll.java
@@ -1,8 +1,13 @@
 package com.greenfox.nezih.springproject;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.awt.*;
 import java.util.Random;
-
+@Controller
 public class HelloAll {
     String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj",
             "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour","Guten Tag", "Gia'sou",
@@ -13,8 +18,24 @@ public class HelloAll {
             "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
 
 
-    public int randomcolor (){
-        Random rnd = new Random();
-        
+
+    @RequestMapping("/web")
+    public String greeting(Model model) {
+        model.addAttribute("greeting", randomPicker(hellos));
+        model.addAttribute("color", randomcolor());
+        return "GreetAll";
     }
+    public String randomcolor (){
+        int R = (int)(Math.random()*256);
+        int G = (int)(Math.random()*256);
+        int B= (int)(Math.random()*256);
+        String color = "color:rgb(" + R + "," + G + "," + B + ");";
+        return color;
+    }
+    public String randomPicker(String[] hellos){
+        Random random = new Random();
+        return hellos[random.nextInt(hellos.length-1)];
+    }
+
+
 }
