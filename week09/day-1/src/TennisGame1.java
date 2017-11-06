a7b9e7993d36a4eb379f3f95c2b4b9a4d0c096bc
nezih <nezihcihan@hotmail.com>
2017-11-06 17:31:29 +0100

    game1 is refactored

--- week09/day-1/src/TennisGame1.java
+++ week09/day-1/src/TennisGame1.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-
 import static java.lang.Math.abs;
 
 public class TennisGame1 implements TennisGame {
@@ -14,20 +13,16 @@ public class TennisGame1 implements TennisGame {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
     }
-
     public void wonPoint(String playerName) {
         if (playerName == "player1")
             m_score1 += 1;
         else
             m_score2 += 1;
     }
-
     public String draw(int score) {
-        String sameScore;
         ArrayList<String> result = new ArrayList<>(Arrays.asList("Love-All","Fifteen-All","Thirty-All","Forty-All","Deuce"));
         return result.get(score);
     }
-
     public String afterFourPoints(String player) {
         String difference ="";
         int minusResult = abs(m_score1-m_score2);
@@ -39,65 +34,21 @@ public class TennisGame1 implements TennisGame {
         }
         return  difference.concat(player);
     }
-
     public String beforeFourPoints(){
-        String result = "";
         ArrayList<String> resultList = new ArrayList<>(Arrays.asList("Love","Fifteen","Thirty", "Forty"));
-        for (int i=1; i<3; i++) {
-//            if (i==1) tempScore = m_score1;
-//            else { score+="-"; tempScore = m_score2;}
-////            switch(tempScore)
-////            {
-////                case 0:
-////                    score+="Love";
-////                    break;
-////                case 1:
-////                    score+="Fifteen";
-////                    break;
-////                case 2:
-////                    score+="Thirty";
-////                    break;
-////                case 3:
-////                    score+="Forty";
-////                    break;
-////            }
+        String result = resultList.get(m_score1) + "-" + resultList.get(m_score2);
+        return  result;
         }
-        String result;
-    }
-
     public String getScore() {
-        String score = "";
-        int tempScore;
-
         if (m_score1==m_score2) {
             return draw(m_score1);
         }
-
         else if (m_score1>=4 || m_score2>=4) {
             String player = m_score1 > m_score2 ? "player1" : "player2";
             return afterFourPoints(player);
         }
         else {
-//            for (int i=1; i<3; i++) {
-//                if (i==1) tempScore = m_score1;
-//                else { score+="-"; tempScore = m_score2;}
-//                switch(tempScore)
-//                {
-//                    case 0:
-//                        score+="Love";
-//                        break;
-//                    case 1:
-//                        score+="Fifteen";
-//                        break;
-//                    case 2:
-//                        score+="Thirty";
-//                        break;
-//                    case 3:
-//                        score+="Forty";
-//                        break;
-//                }
-//            }
+            return beforeFourPoints();
         }
-        return score;
     }
 }
